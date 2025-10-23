4517b4bb218583286ac63276f63fd6587071c08f
AbdulMuqeet13 <malikabdul274.am@gmail.com>
2025-10-23 09:37:08 +0500

    fixes

--- public/build/assets/CardTitle.vue_vue_type_script_setup_true_lang-C4nYmjSs.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./createLucideIcon-CHTgFHG4.js";import{d as t,k as n,v as r,D as l,u as c,o as m}from"./app-aUFiA7hq.js";const f=t({__name:"CardTitle",props:{class:{}},setup(s){const a=s;return(e,p)=>(m(),n("h3",{"data-slot":"card-title",class:l(c(o)("leading-none font-semibold",a.class))},[r(e.$slots,"default")],2))}});export{f as _};
