11d92a176a959ecebd817030761bbdbb2ac51c2f
AbdulMuqeet13 <malikabdul274.am@gmail.com>
2025-10-30 15:24:09 +0500

    fixes

--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,8 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:check-and-promote-cto')->daily()->at('11:00');
-Schedule::command('roi:distribute-cto')->daily()->at('11:30');
-Schedule::command('roi:credit')->weekdays()->dailyAt('12:00');
-Schedule::command('level:income')->weekdays()->dailyAt('12:15');
+Schedule::command('app:check-and-promote-cto')->daily();
+Schedule::command('roi:distribute-cto')->daily();
+Schedule::command('roi:credit')->daily();
+Schedule::command('level:income')->daily();
+Schedule::command('app:test-command')->everyMinute();
